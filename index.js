aad2d699d1293ed2b18b69c394bc3124cd7694dd
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-13 10:24:06 +0600

    remove spacific item from local storage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const storeLocal=(name)=>{
     const inputValue=document.getElementById(name).value;
     localStorage.setItem(name,inputValue)
 }
-
+const removeLocal=(name)=>{
+    localStorage.removeItem(name)
+}
 
 const reset=()=>localStorage.clear();
