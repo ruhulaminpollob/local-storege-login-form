7599b541ff84e77da4773640a6d99819615c423f
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-13 11:07:58 +0600

    added send to save all data as a object to local storage

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ const removeLocal=(name)=>{
     localStorage.removeItem(name)
 }
 
+
+
+const sendLocal=()=>{
+    let obj={};
+    const inputName=document.getElementById('name').value;
+    const inputEmail=document.getElementById('email').value;
+    const inputMessage=document.getElementById('message').value;
+
+    obj['name']=inputName;
+    obj['email']=inputEmail;
+    obj['message']=inputMessage;
+    localStorage.setItem('info',JSON.stringify(obj))
+}
+
 const reset=()=>localStorage.clear();
